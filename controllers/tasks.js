1b20d24b2447256d43d35be9f770f1ba14760505
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-07-26 23:39:35 +0530

    mongoDb

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("create task");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("get all tasks");
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
---
+++ db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://hriday:<password>@nodeexpressprojects.mrlff8j.mongodb.net/?retryWrites=true&w=majority";
