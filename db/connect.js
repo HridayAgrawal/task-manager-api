e016efb6f6c7bda00ffdc9ffbd9a4562279e03bf
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-07-27 23:57:21 +0530

    mongoose

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,11 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://hriday:<password>@nodeexpressprojects.mrlff8j.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://hriday:Hriday1234@nodeexpressprojects.mrlff8j.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connceted to the db...");
+  })
+  .catch((err) => console.log(err));
