9912e51e456baf8b16b33c812a139495a9411a3b
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-07-31 23:56:10 +0530

    updateTask

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDb = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,14 @@
 const Task = require("../models/Task");
-const getAllTasks = async (req, res) => {
+const asyncWrapper = require("../middleware/async");
+
+const getAllTasks = asyncWrapper(async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({ tasks: tasks });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
-};
+});
 
 const createTask = async (req, res) => {
   try {
@@ -32,14 +34,26 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
-};
-
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ID : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
     if (!task) {
       return res.status(404).json({ msg: `No task with ID : ${taskID}` });
     }
