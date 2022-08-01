c35295759a4ecb532dfcb4fda6616b23c86a0cfe
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-08-01 18:59:34 +0530

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const tasks = require("./routes/tasks");
 const connectDb = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.static("./public"));
 app.use(express.json());
@@ -12,8 +13,9 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
